e4bf8561e2e4c95d2337bc953b8312015dd06dce
mickey <damapongm@gmail.com>
2022-04-11 13:37:55 -0700

    add photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    title: `TeslaToday`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: `TeslaToday`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 const IndexPage = () => {
 	return (
 		<Layout pageTitle='Home Page'>
 			<p>I'm making this by following the Gatsby Tutorial.</p>
+			<StaticImage
+				alt='Tesla delivers 65,814 China-made vehicles in March, up 16% from the previous month.'
+				src='https://images.barrons.com/im-336622/social'
+			/>
 		</Layout>
 	);
 };
